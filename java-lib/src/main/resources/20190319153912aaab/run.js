1870f87f7ac205c7e9fde880d04274becd7c5fd6
Jesse Wilson <jwilson@squareup.com>
2019-03-19 16:09:28 -0700

    web app draft

---
+++ java-lib/src/main/resources/20190319153912aaab/run.js
@@ -0,0 +1,24 @@
+window.runs["20190319153912aaab"] = {
+  "shots": [
+      {
+          "file": "btc.webp",
+          "test": "com.squareup.cash.boost.BoostView#active",
+          "timestamp": "2019-03-14T16:13:48Z",
+          "tags": []
+      },
+      {
+          "name": "idle",
+          "file": "card.webp",
+          "test": "com.squareup.cash.boost.BoostView#active",
+          "timestamp": "2019-03-14T16:13:48Z",
+          "tags": ["mooncake"]
+      },
+      {
+          "name": "idle",
+          "file": "boost.webp",
+          "test": "com.squareup.cash.boost.BoostView#active",
+          "timestamp": "2019-03-14T16:13:48Z",
+          "tags": ["mooncake"]
+      }
+  ]
+}
