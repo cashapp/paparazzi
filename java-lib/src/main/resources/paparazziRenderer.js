229dce60a40e0245046766ad5fffcda2d46eabc3
Benoit Quenaudon <bquenaudon@squareup.com>
2019-03-20 14:03:25 -0700

    Cleaning up paparazziRenderer for relay

--- java-lib/src/main/resources/paparazziRenderer.js
+++ java-lib/src/main/resources/paparazziRenderer.js
@@ -1,67 +1,105 @@
 class Run {
-  constructor(id, shots) {
+  constructor(id, data) {
     this.id = id
-    this.shots = shots
+    // TODO(oldergod) which entries are required/optional?
+    this.data = data // These are not Shot object..., they are the run.js.
   }
 }
 
-function deleteme() {
-  const obj = {
-    'shot': {
-      'name': 'idle',
-      'method': "active",
-      'class': "BoostView",
-      'package': "com.squareup.cash.boost",
-      'runs': [
-        {
-          'run': '20190319153912aaab',
-          'file': 'boost.webp',
-          'timestamp': '16:13:48',
-        },
-        {
-          'run': '20190319153912cced',
-          'file': 'boost.webp',
-          'timestamp': '16:13:48'
-        },
-        {
-          'run': '20190319153912oooz',
-          'file': 'boost.webp',
-          'timestamp': '16:13:48'
-        }
-      ]
-    }
-  }
-}
+// Data representation of a Shot.
+// 'shot': {
+//   'name': 'idle',
+//   'method': "active",
+//   'class': "BoostView",
+//   'package': "com.squareup.cash.boost",
+//   'runs': [
+//     {
+//       'run': '20190319153912aaab',
+//       'file': 'boost.webp',
+//       'timestamp': '16:13:48',
+//     },
+//     {
+//       'run': '20190319153912cced',
+//       'file': 'boost.webp',
+//       'timestamp': '16:13:48'
+//     },
+//     {
+//       'run': '20190319153912oooz',
+//       'file': 'boost.webp',
+//       'timestamp': '16:13:48'
+//     }
+//   ]
+// }
 
 class Shot {
   constructor(name, test) {
     this.name = name
     this.test = test // split into method, class, package
+    this.method = "TODO"
+    // TODO(oldergod) is it a keyword?
+    this.class = "TODO"
+    this.package = "TODO"
     this.runs = []
   }
 
-  add(runId, file, timestamp) {
+  addRun(runId, file, timestamp) {
+    // TODO(oldergod) do we need to sort?
     this.runs.push(
       {
-        'run': runId,
+        'id': runId,
         'file': file,
-        'timestamp': timestamp // parse?
+        'timestamp': timestamp
       }
     )
   }
 
+  removeRun(runId) {
+    const index = this.runs.indexOf((run) => run.id == runId)
+    if (index == -1) return
+
+    this.runs.splice(index, 1)
+  }
+
   inflate() {
-    // create div.screen
-    // set img
-    // set test details
-    // add circles
+    const circles = this.runs.map(run => {
+      const circle = document.createElement('div')
+      circle.classList.add('test__details__selector', `run-${run.id}`)
+      circle
+    })
+
+    const nameP = document.createElement('p')
+    nameP.classList.add('test__details', 'test__details__name')
+    nameP.innerText = `${this.method} ${this.name}`
+
+    // TODO create classP
+    // TODO create packageP
+    // TODO create timestampP
+
+    // TODO create overlayDiv
+
+    overlayDiv.appendChild(nameP)
+    overlayDiv.appendChild(classP)
+    overlayDiv.appendChild(packageP)
+    overlayDiv.appendChild(timestampP)
+    circles.forEach(circle => overlayDiv.appendChild(circle))
+
+    // TODO create img
+
+    const div = document.createElement('div')
+    div.classList.add('screen')
+
+    div.appendChild(img)
+    div.appendChild(overlayDiv)
   }
 }
 
 class PaparazziRenderer {
   constructor() {
+    // Used for content comparison for we only re-render the updated ones.
     this.currentRuns = {}
+    // Used to store runs we know won't be updated anymore.
     this.lockedRunIds = []
+    this.shots = {} // Key is `${test}${name}`, Value is a Shot.
   }
 
   start() {
@@ -69,16 +107,29 @@ class PaparazziRenderer {
       this.loadRunScript(runId)
     }
     setInterval(this.refresh.bind(this), 1000)
-    // refresh only last run and rerender
   }
 
   render(run) {
     if (this.currentRuns[run.id]
       && JSON.stringify(this.currentRuns[run.id]) == JSON.stringify(run)) {
+      // This run didn't change.
       return
     }
     this.currentRuns[run.id] = run
     console.log('rendering', run)
+
+    for (let datum of run.data) {
+      const key = `${datum.test}${datum.name}`
+      let shot = this.shots[key]
+      if (!shot) { this.shots[key] = new Shot(datum.name, datum.test) }
+      shot.removeRun(run.id)
+      shot.addRun(run.id, datum.file, datum.timestamp)
+
+      console.log('Gonna render', shot)
+      // inflate
+      // add to html in a idempotent way (we replace first, fallback to appending)
+      // TODO setup listeners for filters/hovering, etc
+    }
   }
 
   renderAll() {
@@ -86,12 +137,14 @@ class PaparazziRenderer {
       if (this.lockedRunIds.includes(runId)) {
         continue
       }
+      // The js loading is async so the rendering can happen in the next refresh
       this.loadRunScript(runId)
 
       this.render(new Run(runId, window.runs[runId]))
 
       const lastRunId = window.all_runs[window.all_runs.length - 1]
       if (runId != lastRunId) {
+        // This run isn't the last run so we know it ain't gonna be updated.
         this.lockedRunIds.push(runId)
         delete this.currentRuns[runId]
       }
