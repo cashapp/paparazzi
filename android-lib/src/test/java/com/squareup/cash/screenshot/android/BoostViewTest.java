22d781edfff22d633195954972922e5574e22f9d
John Rodriguez <john.rodriguez@gmail.com>
2019-03-20 12:43:17 -0700

    Clean up the API

--- android-lib/src/test/java/com/squareup/cash/screenshot/android/BoostViewTest.java
+++ android-lib/src/test/java/com/squareup/cash/screenshot/android/BoostViewTest.java
@@ -1,46 +1,27 @@
 package com.squareup.cash.screenshot.android;
 
-import com.android.ide.common.rendering.api.SessionParams;
-import com.android.ide.common.rendering.api.SessionParams.RenderingMode;
+import android.widget.Button;
 import com.android.layoutlib.bridge.intensive.RenderTestBase;
 import com.android.layoutlib.bridge.intensive.setup.LayoutLibTestCallback;
-import com.android.layoutlib.bridge.intensive.setup.LayoutPullParser;
+import com.squareup.cash.screenshot.R;
+import com.squareup.cash.screenshot.jvm.Paparazzi;
+import org.junit.Rule;
 import org.junit.Test;
 
-import java.util.concurrent.TimeUnit;
-
 public class BoostViewTest extends RenderTestBase {
+    @Rule
+    public Paparazzi paparazzi = new Paparazzi();
 
-    /**
-     * Test indeterminate_progressbar.xml
-     */
     @Test
-    public void testVectorDrawable() throws ClassNotFoundException {
-        // Create the layout pull parser.
-        LayoutPullParser parser = LayoutPullParser.createFromString(
-                "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                        "<LinearLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
-                        "              android:orientation=\"vertical\"\n" +
-                        "              android:layout_width=\"match_parent\"\n" +
-                        "              android:layout_height=\"match_parent\">\n" +
-                        "    <Button\n" +
-                        "            android:background=\"@android:color/holo_orange_dark\"\n" +
-                        "            android:layout_width=\"match_parent\"\n" +
-                        "            android:layout_height=\"wrap_content\"\n" +
-                        "            android:text=\"OMG IT WORKS\"/>\n" +
-                        "</LinearLayout>\n");
-        // Create LayoutLibCallback.
-        LayoutLibTestCallback layoutLibCallback =
-                new LayoutLibTestCallback(getLogger(), mDefaultClassLoader);
-        layoutLibCallback.initResources();
+    public void testVectorDrawable() {
+        Button rootView = paparazzi.inflateView(
+            R.layout.button,
+            new LayoutLibTestCallback(getLogger(), mDefaultClassLoader),
+            getSessionParamsBuilder()
+        );
 
-        SessionParams params = getSessionParamsBuilder()
-                .setParser(parser)
-                .setCallback(layoutLibCallback)
-                .setTheme("Theme.Material.NoActionBar.Fullscreen", false)
-                .setRenderingMode(RenderingMode.V_SCROLL)
-                .build();
+        rootView.setText("Fuck yeeaaa");
 
-        renderAndVerify(params, "vector_drawable.png", TimeUnit.SECONDS.toNanos(2));
+        paparazzi.snapshot();
     }
 }
