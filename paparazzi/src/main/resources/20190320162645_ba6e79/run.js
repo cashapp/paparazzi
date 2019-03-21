2f0beb05528b6e9507df6b2800bad483c8af5958
Jesse Wilson <jwilson@squareup.com>
2019-03-21 10:18:00 -0700

    Rename java-lib to paparazzi and android-lib to sample

---
+++ paparazzi/src/main/resources/20190320162645_ba6e79/run.js
@@ -0,0 +1,23 @@
+window.runs["20190320162645_ba6e79"] = [
+  {
+    "file": "zero_dollars.png",
+    "name": "zero dollars",
+    "tags": [],
+    "testName": "com.squareup.paparazzi.sample.KeypadViewTest#testViews",
+    "timestamp": "2019-03-20T23:26:51.817Z"
+  },
+  {
+    "file": "five_bucks.png",
+    "name": "five bucks",
+    "tags": [],
+    "testName": "com.squareup.paparazzi.sample.KeypadViewTest#testViews",
+    "timestamp": "2019-03-20T23:26:51.862Z"
+  },
+  {
+    "file": "grey.png",
+    "name": "grey",
+    "tags": [],
+    "testName": "com.squareup.paparazzi.sample.KeypadViewTest#testViews",
+    "timestamp": "2019-03-20T23:26:51.893Z"
+  }
+];
---
+++ sample/src/test/java/com/squareup/paparazzi/sample/KeypadViewTest.kt
@@ -0,0 +1,44 @@
+package com.squareup.paparazzi.sample
+
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.squareup.paparazzi.Paparazzi
+import org.junit.Rule
+import org.junit.Test
+
+class KeypadViewTest {
+  @get:Rule
+  var paparazzi = Paparazzi()
+
+  @Test
+  fun testViews() {
+    val keypad = paparazzi.inflate<LinearLayout>(R.layout.keypad)
+    val amount = keypad.findViewById<TextView>(R.id.amount)
+    val amount123 = keypad.findViewById<TextView>(R.id.amount123)
+    val amount456 = keypad.findViewById<TextView>(R.id.amount456)
+    val amount789 = keypad.findViewById<TextView>(R.id.amount789)
+    val amount0 = keypad.findViewById<TextView>(R.id.amount0)
+
+    amount.text = "$0"
+    paparazzi.snapshot(keypad, "zero dollars")
+
+    amount.text = "$5.00"
+    paparazzi.snapshot(keypad, "five bucks")
+
+    keypad.setBackgroundResource(R.color.keypadDarkGrey)
+    val darkGrey = paparazzi.resources.getColor(R.color.keypadDarkGrey)
+    keypad.setBackgroundColor(darkGrey)
+    amount.text = "$1.00"
+    paparazzi.snapshot(keypad, "grey")
+
+    keypad.setBackgroundResource(R.color.keypadDarkGrey)
+    keypad.setBackgroundColor(paparazzi.resources.getColor(R.color.bolt))
+    amount.setTextColor(darkGrey)
+    amount123.setTextColor(darkGrey)
+    amount456.setTextColor(darkGrey)
+    amount789.setTextColor(darkGrey)
+    amount0.setTextColor(darkGrey)
+    amount.text = ".01 BTC"
+    paparazzi.snapshot(keypad, "bolt")
+  }
+}
