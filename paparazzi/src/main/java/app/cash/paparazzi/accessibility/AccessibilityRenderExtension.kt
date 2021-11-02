23dbdd97515d6702587735eb07bc3907290fccb6
Geoff Powell <gpowell@squareup.com>
2021-11-02 10:36:19 -0400

    Attempt to fix font scaling issue by providing bundled monospace font

--- paparazzi/src/main/java/app/cash/paparazzi/accessibility/AccessibilityRenderExtension.kt
+++ paparazzi/src/main/java/app/cash/paparazzi/accessibility/AccessibilityRenderExtension.kt
@@ -15,6 +15,7 @@
  */
 package app.cash.paparazzi.accessibility
 
+import android.graphics.fonts.SystemFonts
 import android.view.View
 import android.view.ViewGroup
 import app.cash.paparazzi.RenderExtension
@@ -24,10 +25,20 @@ import app.cash.paparazzi.accessibility.RenderSettings.DEFAULT_RECT_SIZE
 import app.cash.paparazzi.accessibility.RenderSettings.DEFAULT_TEXT_COLOR
 import app.cash.paparazzi.accessibility.RenderSettings.DEFAULT_TEXT_SIZE
 import app.cash.paparazzi.accessibility.RenderSettings.getColor
+import java.awt.Font
 import java.awt.image.BufferedImage
 
 class AccessibilityRenderExtension : RenderExtension {
   private val accessibilityTree = LinkedHashSet<View>()
+
+  // We have seen issues with the built in font rendering incorrectly on different OS (Linux, Mac OS, etc.).
+  // We will use a roboto font provided by layoutLib to render accessibility metadata.
+  private val robotoFont: Font by lazy {
+    val inputStream = SystemFonts.getAvailableFonts().first { it.file.name.contains("Roboto-Light.ttf") }.file.inputStream()
+    inputStream.use {
+      Font.createFont(Font.TRUETYPE_FONT, it)
+    }
+  }
 
   override fun render(
     snapshot: Snapshot,
@@ -48,27 +59,31 @@ class AccessibilityRenderExtension : RenderExtension {
     val canvasImage = BufferedImage(image.width * 2, image.height, image.type)
 
     val graphics = canvasImage.createGraphics()
+    graphics.font = robotoFont.deriveFont(DEFAULT_TEXT_SIZE)
     graphics.drawImage(image, 0, 0, null)
     graphics.color = DEFAULT_DESCRIPTION_COLOR
     graphics.fillRect(image.width, 0, image.width, image.height)
 
     val colorRectSize = DEFAULT_RECT_SIZE
-    graphics.font = graphics.font.deriveFont(DEFAULT_TEXT_SIZE)
-
     val drawStartX = image.width + image.margin()
     accessibilityTree.forEachIndexed { index, view ->
       val drawStartY = (index * colorRectSize * 1.5f).toInt() + image.margin()
 
-      graphics.color = getColor(view)
-      graphics.fillRoundRect(drawStartX, drawStartY, colorRectSize, colorRectSize,
-          colorRectSize / 4, colorRectSize / 4)
+      val viewColor = getColor(view)
+      graphics.color = viewColor
+      graphics.fillRoundRect(
+        drawStartX, drawStartY, colorRectSize, colorRectSize,
+        colorRectSize / 4, colorRectSize / 4
+      )
       graphics.color = DEFAULT_TEXT_COLOR
       val accessibilityText = view.iterableTextForAccessibility.toString()
 
-      view.createAccessibilityNodeInfo()
-      graphics.drawString(accessibilityText,
-          drawStartX + colorRectSize + image.margin(),
-          drawStartY + colorRectSize / 2 + graphics.fontMetrics.height / 2)
+      val lineMetrics = graphics.fontMetrics.getLineMetrics(accessibilityText, graphics)
+      graphics.drawString(
+        accessibilityText,
+        (drawStartX + colorRectSize + image.margin()).toFloat(),
+        drawStartY + colorRectSize / 3 + lineMetrics.height / 2f
+      )
     }
     return canvasImage
   }
