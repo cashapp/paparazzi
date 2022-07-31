b298e1f60965b93517badfdc89303ecf5d7781ab
Bren Pearson <bren.pearson0100@gmail.com>
2022-07-31 23:21:17 +1000

    Create the snapshot directory if it doesn't exist

--- paparazzi/paparazzi-gradle-plugin/src/main/java/app/cash/paparazzi/gradle/PaparazziPlugin.kt
+++ paparazzi/paparazzi-gradle-plugin/src/main/java/app/cash/paparazzi/gradle/PaparazziPlugin.kt
@@ -87,6 +87,7 @@ class PaparazziPlugin : Plugin<Project> {
       val snapshotOutputDir =
           extension.snapshotRootDirectory.orNull
           ?: project.layout.projectDirectory.dir("src/test/snapshots").asFile
+      snapshotOutputDir.mkdirs()
 
       val packageAwareArtifacts = project.configurations
         .getByName("${variant.name}RuntimeClasspath")
