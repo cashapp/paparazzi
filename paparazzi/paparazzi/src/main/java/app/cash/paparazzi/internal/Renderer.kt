6940686a44ba824955a37e25013b64e9d92043fe
Luis Cortes <luiscortes@squareup.com>
2022-10-18 12:18:54 -0400

    WIP

--- paparazzi/paparazzi/src/main/java/app/cash/paparazzi/internal/Renderer.kt
+++ paparazzi/paparazzi/src/main/java/app/cash/paparazzi/internal/Renderer.kt
@@ -19,8 +19,6 @@ package app.cash.paparazzi.internal
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Environment
 import app.cash.paparazzi.Flags
-import app.cash.paparazzi.internal.parsers.LayoutPullParser
-import com.android.ide.common.rendering.api.SessionParams
 import com.android.ide.common.resources.deprecated.FrameworkResources
 import com.android.ide.common.resources.deprecated.ResourceItem
 import com.android.ide.common.resources.deprecated.ResourceRepository
@@ -28,18 +26,15 @@ import com.android.io.FolderWrapper
 import com.android.layoutlib.bridge.Bridge
 import com.android.layoutlib.bridge.android.RenderParamsFlags
 import com.android.layoutlib.bridge.impl.DelegateManager
-import java.awt.image.BufferedImage
 import java.io.Closeable
 import java.io.File
-import java.io.IOException
 import java.util.Locale
 
 /** View rendering. */
 internal class Renderer(
   private val environment: Environment,
   private val layoutlibCallback: PaparazziCallback,
-  private val logger: PaparazziLogger,
-  private val maxPercentDifference: Double
+  private val logger: PaparazziLogger
 ) : Closeable {
   private var bridge: Bridge? = null
   private lateinit var sessionParamsBuilder: SessionParamsBuilder
@@ -131,86 +126,5 @@ internal class Renderer(
       println("Objects still linked from the DelegateManager:")
       DelegateManager.dump(System.out)
     }
-  }
-
-  fun render(
-    bridge: com.android.ide.common.rendering.api.Bridge,
-    params: SessionParams,
-    frameTimeNanos: Long
-  ): RenderResult {
-    val session = bridge.createSession(params)
-
-    try {
-      if (frameTimeNanos != -1L) {
-        session.setElapsedFrameTimeNanos(frameTimeNanos)
-      }
-
-      if (!session.result.isSuccess) {
-        logger.error(session.result.exception, session.result.errorMessage)
-      } else {
-        // Render the session with a timeout of 50s.
-        val renderResult = session.render(50000)
-        if (!renderResult.isSuccess) {
-          logger.error(session.result.exception, session.result.errorMessage)
-        }
-      }
-
-      return session.toResult()
-    } finally {
-      session.dispose()
-    }
-  }
-
-  /** Compares the golden image with the passed image. */
-  fun verify(
-    goldenImageName: String,
-    image: BufferedImage
-  ) {
-    try {
-      val goldenImagePath = environment.appTestDir + "/golden/" + goldenImageName
-      ImageUtils.requireSimilar(goldenImagePath, image, maxPercentDifference)
-    } catch (e: IOException) {
-      logger.error(e, e.message)
-    }
-  }
-
-  /**
-   * Create a new rendering session and test that rendering the given layout doesn't throw any
-   * exceptions and matches the provided image.
-   *
-   * If frameTimeNanos is >= 0 a frame will be executed during the rendering. The time indicates
-   * how far in the future is.
-   */
-  @JvmOverloads
-  fun renderAndVerify(
-    sessionParams: SessionParams,
-    goldenFileName: String,
-    frameTimeNanos: Long = -1
-  ): RenderResult {
-    val result = render(bridge!!, sessionParams, frameTimeNanos)
-    verify(goldenFileName, result.image)
-    return result
-  }
-
-  fun createParserFromPath(layoutPath: String): LayoutPullParser =
-    LayoutPullParser.createFromPath("${environment.resDir}/layout/$layoutPath")
-
-  /**
-   * Create a new rendering session and test that rendering the given layout on given device
-   * doesn't throw any exceptions and matches the provided image.
-   */
-  @JvmOverloads
-  fun renderAndVerify(
-    layoutFileName: String,
-    goldenFileName: String,
-    deviceConfig: DeviceConfig = DeviceConfig.NEXUS_5
-  ): RenderResult {
-    val sessionParams = sessionParamsBuilder
-      .copy(
-        layoutPullParser = createParserFromPath(layoutFileName),
-        deviceConfig = deviceConfig
-      )
-      .build()
-    return renderAndVerify(sessionParams, goldenFileName)
   }
 }
