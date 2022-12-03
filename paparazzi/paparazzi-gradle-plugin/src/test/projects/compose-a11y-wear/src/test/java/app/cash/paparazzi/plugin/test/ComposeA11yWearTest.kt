040552856019d763d16cd34d601484de48397eaa
Yuri Schimke <yuri@schimke.ee>
2022-12-03 11:21:46 +0000

    Fix dependencies

--- paparazzi/paparazzi-gradle-plugin/src/test/projects/compose-a11y-wear/src/test/java/app/cash/paparazzi/plugin/test/ComposeA11yWearTest.kt
+++ paparazzi/paparazzi-gradle-plugin/src/test/projects/compose-a11y-wear/src/test/java/app/cash/paparazzi/plugin/test/ComposeA11yWearTest.kt
@@ -2,11 +2,11 @@ package app.cash.paparazzi.plugin.test
 
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
+import app.cash.paparazzi.Paparazzi.Companion.determineHandler
+import app.cash.paparazzi.accessibility.A11ySnapshotHandler
+import app.cash.paparazzi.accessibility.ComposeA11yExtension
 import org.junit.Rule
 import org.junit.Test
-import app.cash.paparazzi.accessibility.ComposeA11yExtension
-import app.cash.paparazzi.accessibility.A11ySnapshotHandler
-import app.cash.paparazzi.Paparazzi.Companion.determineHandler
 
 class ComposeA11yWearTest {
   private val composeA11yExtension = ComposeA11yExtension()
@@ -24,7 +24,6 @@ class ComposeA11yWearTest {
       accessibilityStateFn = { composeA11yExtension.accessibilityState }
     )
   )
-
 
   @Test
   fun compose() {
