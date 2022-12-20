0bd0f8cca33260fa26cc0a52169aa4da31dd57c2
Yuri Schimke <yuri@schimke.ee>
2022-12-20 17:31:16 +1000

    Updates

--- paparazzi/paparazzi-gradle-plugin/src/test/projects/wear-gif/src/test/java/app/cash/paparazzi/plugin/test/WearGifTest.kt
+++ paparazzi/paparazzi-gradle-plugin/src/test/projects/wear-gif/src/test/java/app/cash/paparazzi/plugin/test/WearGifTest.kt
@@ -2,14 +2,9 @@ package app.cash.paparazzi.plugin.test
 
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
-import com.android.resources.NightMode
-import com.google.testing.junit.testparameterinjector.TestParameter
-import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(TestParameterInjector::class)
 class WearGifTest {
   @get:Rule
   val paparazzi = Paparazzi(
