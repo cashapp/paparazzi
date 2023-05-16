6775356023dd180df8393bfd218cec171fc7b7b0
Geoff Powell <gpowell@squareup.com>
2023-05-16 12:13:38 -0400

    Use accessibilityText as an identifier instead of id as it's backed by AtomicInteger and isn't consistant across runs

--- paparazzi/paparazzi-gradle-plugin/src/test/projects/compose-a11y/src/test/java/app/cash/paparazzi/plugin/test/ComposeA11yTest.kt
+++ paparazzi/paparazzi-gradle-plugin/src/test/projects/compose-a11y/src/test/java/app/cash/paparazzi/plugin/test/ComposeA11yTest.kt
@@ -1,5 +1,11 @@
 package app.cash.paparazzi.plugin.test
 
+import android.widget.LinearLayout
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
 import app.cash.paparazzi.accessibility.AccessibilityRenderExtension
@@ -15,8 +21,25 @@ class ComposeA11yTest {
   )
 
   @Test
-  fun mixedComposeUsage() {
+  fun `mixed compose usage`() {
     val mixedView = MixedView(paparazzi.context)
+    paparazzi.snapshot(mixedView)
+  }
+
+  @Test
+  fun `verify changing view hierarchy order doesn't change accessibility colors`() {
+    val mixedView = MixedView(paparazzi.context).apply {
+      addView(
+        ComposeView(context).apply {
+          id = 10
+          setContent {
+            Box(modifier = Modifier.size(50.dp)) {}
+          }
+        },
+        0,
+        LinearLayout.LayoutParams(0, 0)
+      )
+    }
     paparazzi.snapshot(mixedView)
   }
 }
