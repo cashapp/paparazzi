1c70f45d811646b1b158fd6f7863dc80bea3ae50
John Rodriguez <jrod@squareup.com>
2023-05-25 00:32:34 -0400

    Merge BasicResourceItem and BasicResourceItemBase again

--- paparazzi/paparazzi/src/main/java/app/cash/paparazzi/internal/resources/base/BasicResourceItem.kt
+++ paparazzi/paparazzi/src/main/java/app/cash/paparazzi/internal/resources/base/BasicResourceItem.kt
@@ -15,12 +15,96 @@
  */
 package app.cash.paparazzi.internal.resources.base
 
+import app.cash.paparazzi.internal.resources.RepositoryConfiguration
+import com.android.ide.common.rendering.api.ResourceNamespace
+import com.android.ide.common.rendering.api.ResourceReference
 import com.android.ide.common.rendering.api.ResourceValue
 import com.android.ide.common.resources.ResourceItemWithVisibility
+import com.android.resources.ResourceType
+import com.android.resources.ResourceVisibility
+import com.android.utils.HashCodes
+import com.google.common.base.MoreObjects
 
 /**
- * Ported from: [BasicResourceItem.java](https://cs.android.com/android-studio/platform/tools/base/+/47d204001bf0cb6273d8b135c7eece3a982cf0e0:resource-repository/main/java/com/android/resources/base/BasicResourceItem.java)
- *
- * Combination of [ResourceItemWithVisibility] and [ResourceValue] interfaces.
+ * Ported from: [BasicResourceItemBase.java](https://cs.android.com/android-studio/platform/tools/base/+/18047faf69512736b8ddb1f6a6785f58d47c893f:resource-repository/main/java/com/android/resources/base/BasicResourceItemBase.java)
  */
-interface BasicResourceItem : ResourceItemWithVisibility, ResourceValue
+abstract class BasicResourceItem(
+  private val type: ResourceType,
+  private val name: String,
+  visibility: ResourceVisibility
+) : ResourceItemWithVisibility, ResourceValue {
+  // Store enums as their ordinals in byte form to minimize memory footprint.
+  private val typeOrdinal: Byte = type.ordinal.toByte()
+  private val visibilityOrdinal: Byte = visibility.ordinal.toByte()
+
+  override fun getType() = resourceType
+
+  override fun getNamespace(): ResourceNamespace = repository.namespace
+
+  override fun getName() = name
+
+  override fun getLibraryName() = repository.libraryName
+
+  override fun getResourceType() = ResourceType.values()[typeOrdinal.toInt()]
+
+  override fun getVisibility() = ResourceVisibility.values()[visibilityOrdinal.toInt()]
+
+  override fun getReferenceToSelf() = asReference()
+
+  override fun getResourceValue() = this
+
+  override fun isUserDefined() = repository.containsUserDefinedResources()
+
+  override fun isFramework() = namespace == ResourceNamespace.ANDROID
+
+  override fun asReference() = ResourceReference(namespace, resourceType, name)
+
+  /**
+   * Returns the repository this resource belongs to.
+   *
+   *
+   * Framework resource items may move between repositories with the same origin.
+   * @see RepositoryConfiguration.transferOwnershipTo
+   */
+  override fun getRepository() = getRepositoryConfiguration().repository
+
+  override fun getConfiguration() = getRepositoryConfiguration().folderConfiguration
+
+  abstract fun getRepositoryConfiguration(): RepositoryConfiguration
+
+  override fun getKey(): String {
+    val qualifiers = configuration.qualifierString
+    return if (qualifiers.isNotEmpty()) {
+      (type.getName() + '-') + qualifiers + '/' + name
+    } else (type.getName() + '/') + name
+  }
+
+  override fun setValue(value: String?) = throw UnsupportedOperationException()
+
+  override fun equals(obj: Any?): Boolean {
+    if (this === obj) {
+      return true
+    }
+    if (obj == null || javaClass != obj.javaClass) {
+      return false
+    }
+
+    val other = obj as BasicResourceItem
+    return typeOrdinal == other.typeOrdinal && name == other.name && visibilityOrdinal == other.visibilityOrdinal
+  }
+
+  override fun hashCode(): Int {
+    // The visibilityOrdinal field is intentionally not included in hash code because having two resource items
+    // differing only by visibility in the same hash table is extremely unlikely.
+    return HashCodes.mix(typeOrdinal.toInt(), name.hashCode())
+  }
+
+  override fun toString(): String {
+    return MoreObjects.toStringHelper(this)
+      .add("namespace", namespace)
+      .add("type", resourceType)
+      .add("name", name)
+      .add("value", value)
+      .toString()
+  }
+}
