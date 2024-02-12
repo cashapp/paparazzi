afab0c4f41f4aa01ec2ea3d8f2000945ff16f040
Brian Gardner <bgardner@squareup.com>
2024-02-12 13:07:29 -0500

    More discrete clip spec properties

--- paparazzi2/src/main/java/app/cash/paparazzi/ClipSpec.kt
+++ paparazzi2/src/main/java/app/cash/paparazzi/ClipSpec.kt
@@ -5,21 +5,27 @@ import java.util.concurrent.TimeUnit
 public data class ClipSpec(
   val frameSpec: FrameSpec,
   val start: Long,
-  val end: Long,
-  val fps: Int
+  val frameDelay: Int,
+  val frameCount: Int
 ) {
-  private val durationMillis: Int
-    get() = (end - start).toInt()
-
-  // Add one to the frame count so we get the last frame. Otherwise a 1 second, 60 FPS animation
-  // our 60th frame will be at time 983 ms, and we want our last frame to be 1,000 ms. This gets
-  // us 61 frames for a 1 second animation, 121 frames for a 2 second animation, etc.
-  private val frameCount: Int
-    get() = (durationMillis * fps) / 1000 + 1
+  public constructor(
+    frameSpec: FrameSpec,
+    start: Long,
+    end: Long,
+    fps: Int
+  ) : this(
+    frameSpec = frameSpec,
+    start = start,
+    frameDelay = 1000 / fps,
+    // Add one to the frame count so we get the last frame. Otherwise a 1 second, 60 FPS animation
+    // our 60th frame will be at time 983 ms, and we want our last frame to be 1,000 ms. This gets
+    // us 61 frames for a 1 second animation, 121 frames for a 2 second animation, etc.
+    frameCount = ((end - start).toInt() * fps) / 1000 + 1
+  )
 
   val frame: Sequence<ClipFrameSpec>
     get() = (0 until frameCount).asSequence().map { frameNumber ->
-      val nowMillis = start + (frameNumber * 1000 / fps)
+      val nowMillis = start + (frameNumber * frameDelay)
       val snapshotTimeNanos = TimeUnit.MILLISECONDS.toNanos(nowMillis)
       ClipFrameSpec(snapshotTimeNanos)
     }
