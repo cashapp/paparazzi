3f256e5fd20072d27d2febcecfba0daa764beabc
Brian Gardner <bgardner@squareup.com>
2024-03-01 16:34:25 -0500

    Split paparazzi into sdk and test rule (#1146)

--- paparazzi/src/test/java/app/cash/paparazzi/PaparazziTest.kt
+++ paparazzi/src/test/java/app/cash/paparazzi/PaparazziTest.kt
@@ -220,6 +220,6 @@ class PaparazziTest {
 
   private val time: Long
     get() {
-      return TimeUnit.NANOSECONDS.toMillis(System_Delegate.nanoTime() - Paparazzi.TIME_OFFSET_NANOS)
+      return TimeUnit.NANOSECONDS.toMillis(System_Delegate.nanoTime() - PaparazziSdk.TIME_OFFSET_NANOS)
     }
 }
