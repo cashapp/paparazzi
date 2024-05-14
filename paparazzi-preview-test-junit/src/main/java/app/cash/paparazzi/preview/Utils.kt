aa8e232e2cc30bdc747ab91b41b0ddae6cdaf715
Geoff Powell <gpowell@squareup.com>
2024-05-14 11:16:43 -0400

    Cleanup preview param

--- paparazzi-preview-test-junit/src/main/java/app/cash/paparazzi/preview/Utils.kt
+++ paparazzi-preview-test-junit/src/main/java/app/cash/paparazzi/preview/Utils.kt
@@ -4,10 +4,7 @@ package app.cash.paparazzi.preview
 import app.cash.paparazzi.Paparazzi
 import app.cash.paparazzi.preview.runtime.PaparazziPreviewData
 
-internal fun Paparazzi.snapshotDefault(
-  previewData: PaparazziPreviewData.Default,
-  name: String?
-) {
+internal fun Paparazzi.snapshotDefault(previewData: PaparazziPreviewData.Default, name: String?) {
   snapshot(name) {
     previewData.composable()
   }
