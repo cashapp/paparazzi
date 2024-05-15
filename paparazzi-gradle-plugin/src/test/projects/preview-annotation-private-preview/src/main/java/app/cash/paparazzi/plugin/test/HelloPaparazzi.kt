2da32db14bd08957a79d46f72230e96a0190e3ff
Geoff Powell <gpowell@squareup.com>
2024-05-14 20:54:33 -0400

    Add plugin tests for annotation

--- paparazzi-gradle-plugin/src/test/projects/preview-annotation-private-preview/src/main/java/app/cash/paparazzi/plugin/test/HelloPaparazzi.kt
+++ paparazzi-gradle-plugin/src/test/projects/preview-annotation-private-preview/src/main/java/app/cash/paparazzi/plugin/test/HelloPaparazzi.kt
@@ -1,16 +1,21 @@
 package app.cash.paparazzi.plugin.test
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import app.cash.paparazzi.annotations.Paparazzi
 
 @Paparazzi
 @Preview
 @Composable
-fun HelloPaparazzi() {
-  Text("Hello, Paparazzi", style = TextStyle(fontFamily = FontFamily.Cursive), color = Color.White)
-}
+private fun HelloPaparazzi() = Unit
