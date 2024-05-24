4b353e778b78254975ffba8fc2a326e64aeb40c6
John Rodriguez <john.rodriguez@gmail.com>
2024-05-24 16:17:32 +0200

    Reference java.nio.file.NoSuchFileException vs kotlin.io.NoSuchFileException in exception logic for missing public.txt (#1448)

--- paparazzi/src/main/java/app/cash/paparazzi/internal/resources/AarSourceResourceRepository.kt
+++ paparazzi/src/main/java/app/cash/paparazzi/internal/resources/AarSourceResourceRepository.kt
@@ -21,6 +21,7 @@ import java.io.IOException
 import java.io.InputStreamReader
 import java.nio.charset.StandardCharsets.UTF_8
 import java.nio.file.Files
+import java.nio.file.NoSuchFileException
 import java.nio.file.Path
 import java.util.logging.Logger
 import java.util.stream.Collectors
