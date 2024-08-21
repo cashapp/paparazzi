76302907d77b120c932e19789b20dced0c4450be
Geoff Powell <gpowell@squareup.com>
2024-08-21 15:16:30 -0400

    Cleanup from upstream changes

--- paparazzi-gradle-plugin/src/main/java/app/cash/paparazzi/gradle/utils/PreviewUtils.kt
+++ paparazzi-gradle-plugin/src/main/java/app/cash/paparazzi/gradle/utils/PreviewUtils.kt
@@ -1,10 +1,9 @@
 package app.cash.paparazzi.gradle.utils
 
 import app.cash.paparazzi.gradle.PaparazziExtension
+import com.android.build.api.variant.AndroidComponentsExtension
+import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.TestedExtension
-import com.android.build.gradle.api.BaseVariant
-import com.android.build.gradle.internal.api.TestedVariant
-import org.gradle.api.DomainObjectSet
 import org.gradle.api.Project
 import java.io.File
 import java.util.Locale
@@ -14,53 +13,63 @@ private const val KSP_SOURCE_DIR = "build/generated/ksp"
 private const val PREVIEW_DATA_FILE = "paparazziPreviews.kt"
 private const val PREVIEW_TEST_FILE = "PreviewTests.kt"
 
-internal fun <T> Project.registerGeneratePreviewTask(
-  variants: DomainObjectSet<T>,
+private val Project.libraryExtension: LibraryExtension?
+  get() = extensionByTypeOrNull(LibraryExtension::class.java)
+
+private fun <T> Project.extensionByTypeOrNull(cls: Class<T>): T? =
+  try {
+    extensions.getByType(cls)
+  } catch (e: Exception) {
+    null
+  }
+
+internal fun Project.registerGeneratePreviewTask(
   config: PaparazziExtension
-) where T : BaseVariant, T : TestedVariant {
-  val extension = extensions.getByType(TestedExtension::class.java)
-  val namespace = extension.namespace
-  val namespaceDir = namespace?.replace(".", "/")
+) {
+  libraryExtension?.let { library ->
+    library.libraryVariants.all { variant ->
+      val namespace = library.namespace
+      val namespaceDir = namespace?.replace(".", "/")
 
-  variants.all { variant ->
-    val typeName = variant.buildType.name
-    val typeNameCap = typeName.capitalize()
+      val typeName = variant.buildType.name
+      val typeNameCap = typeName.capitalize()
 
-    val testSourceDir = "$projectDir/$TEST_SOURCE_DIR/${typeName}UnitTest"
-    val previewTestDir = "$testSourceDir/$namespaceDir"
+      val testSourceDir = "$projectDir/$TEST_SOURCE_DIR/${typeName}UnitTest"
+      val previewTestDir = "$testSourceDir/$namespaceDir"
 
-    extension.sourceSets.getByName("test$typeNameCap").java {
-      srcDir(testSourceDir)
-    }
+      library.sourceSets.getByName("test$typeNameCap").java {
+        srcDir(testSourceDir)
+      }
+      println("typeName: ${variant.buildType.name} ${library.namespace} $namespaceDir $typeNameCap")
+      if (config.generatePreviewTestClass.get()) {
+        val taskName = "paparazziGeneratePreview${typeNameCap}UnitTestKotlin"
+        tasks.register(taskName) { task ->
+          task.description = "Generates the preview test class to the test source set for $typeName"
 
-    if (config.generatePreviewTestClass.get()) {
-      val taskName = "paparazziGeneratePreview${typeNameCap}UnitTestKotlin"
-      tasks.register(taskName) { task ->
-        task.description = "Generates the preview test class to the test source set for $typeName"
-
-        task.dependsOn("ksp${typeNameCap}Kotlin")
-        task.inputs.file(
-          "$projectDir/$KSP_SOURCE_DIR/$typeName/kotlin/$namespaceDir/$PREVIEW_DATA_FILE"
-        )
-        task.outputs.dir(previewTestDir)
-        task.outputs.file("$previewTestDir/$PREVIEW_TEST_FILE")
-        task.outputs.cacheIf { true }
-
-        task.doLast {
-          File(previewTestDir).mkdirs()
-          File(previewTestDir, PREVIEW_TEST_FILE).writeText(
-            buildString {
-              appendLine("package $namespace")
-              append(PREVIEW_TEST_SOURCE)
-            }
+          task.dependsOn("ksp${typeNameCap}Kotlin")
+          task.inputs.file(
+            "$projectDir/$KSP_SOURCE_DIR/$typeName/kotlin/$namespaceDir/$PREVIEW_DATA_FILE"
           )
+          task.outputs.dir(previewTestDir)
+          task.outputs.file("$previewTestDir/$PREVIEW_TEST_FILE")
+          task.outputs.cacheIf { true }
+
+          // test compilation depends on the task
+          tasks.findByName("compile${typeNameCap}UnitTestKotlin")?.dependsOn(taskName)
+          // run task before processing symbols
+          tasks.findByName("ksp${typeNameCap}UnitTestKotlin")?.mustRunAfter(taskName)
+
+          task.doLast {
+            File(previewTestDir).mkdirs()
+            File(previewTestDir, PREVIEW_TEST_FILE).writeText(
+              buildString {
+                appendLine("package $namespace")
+                append(PREVIEW_TEST_SOURCE)
+              }
+            )
+          }
         }
       }
-
-      // test compilation depends on the task
-      tasks.findByName("compile${typeNameCap}UnitTestKotlin")?.dependsOn(taskName)
-      // run task before processing symbols
-      tasks.findByName("ksp${typeNameCap}UnitTestKotlin")?.mustRunAfter(taskName)
     }
   }
 }
