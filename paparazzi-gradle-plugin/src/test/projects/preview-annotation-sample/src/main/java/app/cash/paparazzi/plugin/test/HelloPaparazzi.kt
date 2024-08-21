fd3d2e9cc1b7683adabaee1c13dca1e9eb1aad6f
Geoff Powell <gpowell@squareup.com>
2024-08-21 15:16:30 -0400

    Cleanup from upstream changes

---
+++ paparazzi-gradle-plugin/src/test/projects/preview-annotation-sample/src/main/java/app/cash/paparazzi/plugin/test/HelloPaparazzi.kt
@@ -0,0 +1,50 @@
+package app.cash.paparazzi.plugin.test
+
+import android.content.res.Configuration
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.tooling.preview.Wallpapers
+import app.cash.paparazzi.annotations.Paparazzi
+
+@Paparazzi
+@Preview
+@Composable
+fun HelloPaparazzi() {
+  Text("Hello, Paparazzi!")
+}
+
+@Paparazzi
+@Preview
+@Composable
+fun HelloPaparazziParameterized(
+  @PreviewParameter(provider = PreviewData::class) text: String,
+) {
+  Text(text)
+}
+
+@Paparazzi
+@Preview(
+  name = "PreviewConfig",
+  group = "Previews",
+  device = "id:Nexus 6",
+  apiLevel = 33,
+  showSystemUi = true,
+  uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL,
+  wallpaper = Wallpapers.RED_DOMINATED_EXAMPLE,
+  showBackground = true,
+  fontScale = 1.5f
+)
+@Composable
+fun HelloPaparazziPreviewConfig() {
+  Text("Hello Paparazzi Preview Config!")
+}
+
+object PreviewData : PreviewParameterProvider<String> {
+  override val values: Sequence<String> = sequenceOf(
+    "Hello, Paparazzi One!",
+    "Hello, Paparazzi Two!",
+  )
+}
