febb3a6be038ad2faa4609b64fd3a1c93186d221
Geoff Powell <gpowell@squareup.com>
2024-08-23 14:15:05 -0400

    Add support for configuration caching

--- paparazzi-gradle-plugin/src/main/java/app/cash/paparazzi/gradle/PreviewTests.kt
+++ paparazzi-gradle-plugin/src/main/java/app/cash/paparazzi/gradle/PreviewTests.kt
@@ -4,8 +4,6 @@ internal const val PREVIEW_TEST_SOURCE = """
 import app.cash.paparazzi.Paparazzi
 import app.cash.paparazzi.annotations.PaparazziPreviewData
 import app.cash.paparazzi.preview.PaparazziValuesProvider
-import app.cash.paparazzi.preview.deviceConfig
-import app.cash.paparazzi.preview.locale
 import app.cash.paparazzi.preview.snapshot
 import com.android.ide.common.rendering.api.SessionParams.RenderingMode.SHRINK
 import com.google.testing.junit.testparameterinjector.TestParameter
@@ -24,13 +22,9 @@ class PreviewTests(
 
   @get:Rule
   val paparazzi = Paparazzi(
-    deviceConfig = preview.deviceConfig(),
     renderingMode = SHRINK,
     maxPercentDifference = 0.11,
   )
-
-  @get:Rule
-  val localeRule = DefaultLocaleRule(preview.locale())
 
   @Test
   fun preview() {
