3a979f9b1365ee7114702cfe4433d5cd7dc7c614
Ben Wicks <benjamin.wicks@aexp.com>
2024-10-09 16:44:56 -0500

    Set delta image's similar pixels to the actual RGB value, to differentiate them from the different pixels
    Addresses issue #1612

---
+++ sample/src/test/java/app/cash/paparazzi/sample/SimilarDeltaTest.kt
@@ -0,0 +1,62 @@
+package app.cash.paparazzi.sample
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import app.cash.paparazzi.Paparazzi
+import org.junit.Rule
+import org.junit.Test
+
+class SimilarDeltaTest {
+  @get:Rule
+  val paparazzi = Paparazzi()
+
+  @Test
+  fun composeSimilarDeltaBoxes() {
+    paparazzi.snapshot { SimilarDeltaBoxes(shouldUseOriginalBackgroundColor = true) }
+  }
+}
+
+@Suppress("TestFunctionName")
+@Composable
+fun SimilarDeltaBoxes(
+  shouldUseOriginalBackgroundColor: Boolean
+) {
+  Column(
+    Modifier
+      .background(Color.White)
+      .fillMaxSize()
+      .wrapContentSize()
+  ) {
+    Box(
+      Modifier
+        .background(if (shouldUseOriginalBackgroundColor) Color.Yellow else Color(0xFFFFFD00))
+        .size(100.dp),
+      contentAlignment = Alignment.Center,
+    ) {
+      Text("Similar")
+    }
+
+    Box(
+      Modifier
+        .background(if (shouldUseOriginalBackgroundColor) Color.Yellow else Color.Red)
+        .size(100.dp),
+      contentAlignment = Alignment.Center,
+    ) {
+      Text("Different")
+    }
+  }
+}
