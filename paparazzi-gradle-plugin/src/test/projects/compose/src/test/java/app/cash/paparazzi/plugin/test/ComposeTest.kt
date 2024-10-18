8724f46444b7c43625c780c8604742caa2e3a543
Geoff Powell <gpowell@squareup.com>
2024-10-18 15:35:56 -0400

    Proposed fix to better time Compose UI animations

--- paparazzi-gradle-plugin/src/test/projects/compose/src/test/java/app/cash/paparazzi/plugin/test/ComposeTest.kt
+++ paparazzi-gradle-plugin/src/test/projects/compose/src/test/java/app/cash/paparazzi/plugin/test/ComposeTest.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
 import app.cash.paparazzi.Paparazzi
 import org.junit.Rule
 import org.junit.Test
@@ -34,5 +35,22 @@ class ComposeTest {
         Text("Hello")
       }
     }
+  }
+
+  @Test
+  fun animation() {
+    val view = ComposeView(paparazzi.context).apply {
+      setContent { SimpleAnimation() }
+    }
+
+    paparazzi.gif(view, fps = 120)
+    paparazzi.gif(view, name = "start-end", fps = 2, end = 500)
+    paparazzi.gif(view, name = "middle-anim", start = 200, fps = 60)
+    paparazzi.snapshot(view = view, offsetMillis = 1, name = "1ms")
+    paparazzi.snapshot(view = view, offsetMillis = 100, name = "100ms")
+    paparazzi.snapshot(view = view, offsetMillis = 200, name = "200ms")
+    paparazzi.snapshot(view = view, offsetMillis = 300, name = "300ms")
+    paparazzi.snapshot(view = view, offsetMillis = 400, name = "400ms")
+    paparazzi.snapshot(view = view, offsetMillis = 500, name = "500ms")
   }
 }
