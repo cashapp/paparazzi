fa131cb401cc4c722833d2d848faa9f454a64822
Ben Wicks <benjamin.wicks@aexp.com>
2024-10-21 22:39:01 -0500

    Set delta image's similar pixels to the blue, to differentiate them from the different pixels
    And add plugin test

--- paparazzi-gradle-plugin/src/test/projects/verify-similar/src/test/java/app/cash/paparazzi/plugin/test/VerifyTest.kt
+++ paparazzi-gradle-plugin/src/test/projects/verify-similar/src/test/java/app/cash/paparazzi/plugin/test/VerifyTest.kt
@@ -1,4 +1,19 @@
-package app.cash.paparazzi.sample
+/*
+ * Copyright (C) 2020 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.paparazzi.plugin.test
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -16,21 +31,19 @@ import app.cash.paparazzi.Paparazzi
 import org.junit.Rule
 import org.junit.Test
 
-class SimilarDeltaTest {
+class VerifyTest {
   @get:Rule
   val paparazzi = Paparazzi()
 
   @Test
-  fun composeSimilarDeltaBoxes() {
-    paparazzi.snapshot { SimilarDeltaBoxes(shouldUseOriginalBackgroundColor = true) }
+  fun verify() {
+    paparazzi.snapshot { SimilarDeltaBoxes() }
   }
 }
 
 @Suppress("TestFunctionName")
 @Composable
-fun SimilarDeltaBoxes(
-  shouldUseOriginalBackgroundColor: Boolean
-) {
+fun SimilarDeltaBoxes() {
   Column(
     Modifier
       .background(Color.White)
@@ -39,7 +52,7 @@ fun SimilarDeltaBoxes(
   ) {
     Box(
       Modifier
-        .background(if (shouldUseOriginalBackgroundColor) Color.Yellow else Color(0xFFFFFD00))
+        .background(Color(0xFFFFFD00))
         .size(100.dp),
       contentAlignment = Alignment.Center
     ) {
@@ -48,7 +61,7 @@ fun SimilarDeltaBoxes(
 
     Box(
       Modifier
-        .background(if (shouldUseOriginalBackgroundColor) Color.Yellow else Color.Red)
+        .background(Color.Red)
         .size(100.dp),
       contentAlignment = Alignment.Center
     ) {
