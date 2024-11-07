be29413b1ef6312582794eb42e9e8fccf35ac170
Geoff Powell <gpowell@squareup.com>
2024-11-07 11:19:56 -0500

    Fix default layout params for compose snapshots

--- paparazzi-gradle-plugin/src/test/projects/compose/src/test/java/app/cash/paparazzi/plugin/test/ComposeTest.kt
+++ paparazzi-gradle-plugin/src/test/projects/compose/src/test/java/app/cash/paparazzi/plugin/test/ComposeTest.kt
@@ -1,5 +1,12 @@
 package app.cash.paparazzi.plugin.test
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import app.cash.paparazzi.Paparazzi
 import org.junit.Rule
 import org.junit.Test
@@ -12,6 +19,20 @@ class ComposeTest {
   fun compose() {
     paparazzi.snapshot {
       HelloPaparazzi()
+    }
+  }
+
+  @Test
+  fun composeDefaultLayoutParams() {
+    paparazzi.snapshot {
+      Box(
+        modifier = Modifier
+          .background(Color.Cyan)
+          .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+      ) {
+        Text("Hello")
+      }
     }
   }
 }
