ba3dcd9b699c22503db26aa33f4e40d3e1bf1ce1
John Rodriguez <john.rodriguez@gmail.com>
2025-06-24 03:34:29 -0400

    Introduce snapshot browser

--- paparazzi/src/test/java/app/cash/paparazzi/HtmlReportWriterTest.kt
+++ paparazzi/src/test/java/app/cash/paparazzi/HtmlReportWriterTest.kt
@@ -60,7 +60,7 @@ class HtmlReportWriterTest {
           name = "loading",
           testName = TestName("app.cash.paparazzi", "CelebrityTest", "testSettings"),
           timestamp = Instant.parse("2019-03-20T10:27:43Z").toDate(),
-          tags = listOf("redesign")
+          tags = mapOf("project" to "redesign")
         ),
         frameCount = 1,
         fps = -1
@@ -85,9 +85,9 @@ class HtmlReportWriterTest {
         |    "name": "loading",
         |    "testName": "app.cash.paparazzi.CelebrityTest#testSettings",
         |    "timestamp": "2019-03-20T10:27:43.000Z",
-        |    "tags": [
-        |      "redesign"
-        |    ],
+        |    "tags": {
+        |      "project": "redesign"
+        |    },
         |    "file": "images/$anyImageHash.png"
         |  }
         |];
@@ -320,7 +320,7 @@ class HtmlReportWriterTest {
           name = "loading",
           testName = TestName("app.cash.paparazzi", "CelebrityTest", "testSettings"),
           timestamp = Instant.parse("2019-03-20T10:27:43Z").toDate(),
-          tags = listOf("redesign")
+          tags = mapOf("project" to "redesign")
         ),
         frameCount = 2,
         fps = 1
@@ -346,9 +346,9 @@ class HtmlReportWriterTest {
         |    "name": "loading",
         |    "testName": "app.cash.paparazzi.CelebrityTest#testSettings",
         |    "timestamp": "2019-03-20T10:27:43.000Z",
-        |    "tags": [
-        |      "redesign"
-        |    ],
+        |    "tags": {
+        |      "project": "redesign"
+        |    },
         |    "file": "videos/$anyVideoHash.png"
         |  }
         |];
