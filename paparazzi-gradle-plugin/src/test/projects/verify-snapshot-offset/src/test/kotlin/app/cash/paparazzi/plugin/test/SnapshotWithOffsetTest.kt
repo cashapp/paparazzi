dc81b96f30799e07461741c23be65361f2a3a6cf
Nisha Toma <nishatoma@gmail.com>
2025-12-29 15:40:16 -0500

    Make new test project for snapshot offset

---
+++ paparazzi-gradle-plugin/src/test/projects/verify-snapshot-offset/src/test/kotlin/app/cash/paparazzi/plugin/test/SnapshotWithOffsetTest.kt
@@ -0,0 +1,58 @@
+package app.cash.paparazzi.plugin.test
+
+import android.content.Context
+import androidx.annotation.RawRes
+import app.cash.paparazzi.Paparazzi
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieComposition
+import com.airbnb.lottie.LottieCompositionFactory
+import org.junit.Rule
+import org.junit.Test
+
+class SnapshotWithOffsetTest {
+
+  @get:Rule
+  var paparazzi = Paparazzi()
+
+  @Test
+  fun `Verify animated view progress`() {
+    val composition = getCompositionLogo(res = R.raw.lottie_logo, context = paparazzi.context)
+
+    val view = getLottieAnimationView(context = paparazzi.context, composition = composition)
+
+    for (offsetInMs in 400..1000 step 200) {
+      paparazzi.snapshot(
+        view = view,
+        name = "at ${offsetInMs}ms",
+        offsetMillis = offsetInMs.toLong()
+      )
+    }
+  }
+
+  @Test
+  fun `Verify animated view completed with large offset`() {
+    val composition = getCompositionLogo(res = R.raw.lottie_logo, context = paparazzi.context)
+
+    val view = getLottieAnimationView(context = paparazzi.context, composition = composition)
+
+    paparazzi.snapshot(view = view, offsetMillis = 5000_000L)
+  }
+
+  @Test
+  fun `Verify animated view blank with negative offset`() {
+    val composition = getCompositionLogo(res = R.raw.lottie_logo, context = paparazzi.context)
+
+    val view = getLottieAnimationView(context = paparazzi.context, composition = composition)
+
+    paparazzi.snapshot(view = view, offsetMillis = -1L)
+  }
+
+  private fun getLottieAnimationView(context: Context, composition: LottieComposition) =
+    LottieAnimationView(context).apply {
+      setComposition(composition)
+      playAnimation()
+    }
+
+  private fun getCompositionLogo(@RawRes res: Int, context: Context) =
+    LottieCompositionFactory.fromRawResSync(context, res).value!!
+}
