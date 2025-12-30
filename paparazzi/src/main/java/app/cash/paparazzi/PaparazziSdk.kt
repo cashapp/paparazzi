b952bac4b4dc7cb5fc01c887f11c6be74a69147c
Nisha Toma <nishatoma@gmail.com>
2025-12-30 17:45:15 -0500

    Add check for fps in gifs

--- paparazzi/src/main/java/app/cash/paparazzi/PaparazziSdk.kt
+++ paparazzi/src/main/java/app/cash/paparazzi/PaparazziSdk.kt
@@ -221,6 +221,9 @@ public class PaparazziSdk @JvmOverloads constructor(
 
   @JvmOverloads
   public fun gif(view: View, start: Long = 0L, end: Long = 500L, fps: Int = 30) {
+    check(fps > -1) {
+      "You must pass a non-negative fps value for gifs."
+    }
     // Add one to the frame count so we get the last frame. Otherwise a 1 second, 60 FPS animation
     // our 60th frame will be at time 983 ms, and we want our last frame to be 1,000 ms. This gets
     // us 61 frames for a 1 second animation, 121 frames for a 2 second animation, etc.
