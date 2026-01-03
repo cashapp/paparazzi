b51e3b74d4e520f9393b1c70fcbc178d54707771
Nisha Toma <nishatoma@gmail.com>
2026-01-03 16:44:05 -0500

    Add test project for TestNg

---
+++ paparazzi-gradle-plugin/src/test/projects/supports-testng/src/test/kotlin/app/cash/paparazzi/plugin/test/TestNgPaparazziTest.kt
@@ -0,0 +1,44 @@
+package app.cash.paparazzi.plugin.test
+
+import android.view.Gravity
+import android.widget.TextView
+import app.cash.paparazzi.Paparazzi
+import app.cash.paparazzi.TestName
+import org.testng.ITestResult
+import org.testng.annotations.AfterMethod
+import org.testng.annotations.BeforeMethod
+import org.testng.annotations.Test
+
+class TestNgPaparazziTest {
+
+  lateinit var paparazzi: Paparazzi
+
+  @BeforeMethod
+  fun setup(result: ITestResult) {
+    val testMethod = result.method
+    val className = testMethod.realClass.simpleName
+    val methodName = testMethod.methodName
+    val packageName = testMethod?.realClass?.`package`?.name.orEmpty()
+
+    val testName = TestName(packageName, className, methodName)
+
+    paparazzi = Paparazzi()
+    paparazzi.beforeTest(testName = testName)
+  }
+
+  @AfterMethod
+  fun tearDown() {
+    paparazzi.afterTest()
+  }
+
+  @Test
+  fun `verify testng snapshot`() {
+    val textView = paparazzi.inflate<TextView>(android.R.layout.simple_list_item_1).apply {
+      text = "Paparazzi TestNg test"
+      textSize = 24f
+      gravity = Gravity.CENTER
+    }
+
+    paparazzi.snapshot(textView)
+  }
+}
