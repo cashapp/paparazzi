86193444599db432202357132c1d7a5e0794c319
Nisha Toma <nishatoma@gmail.com>
2026-01-15 08:52:30 -0500

    Address comments

--- paparazzi-gradle-plugin/src/test/projects/locale-qualifier/src/test/java/app/cash/paparazzi/plugin/test/DefaultLocaleTest.kt
+++ paparazzi-gradle-plugin/src/test/projects/locale-qualifier/src/test/java/app/cash/paparazzi/plugin/test/DefaultLocaleTest.kt
@@ -1,44 +1,42 @@
 package app.cash.paparazzi.plugin.test
 
-import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
 import com.google.testing.junit.testparameterinjector.TestParameter
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
-import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
+import org.junit.runners.model.Statement
 
 @RunWith(TestParameterInjector::class)
 class DefaultLocaleTest(@TestParameter val locale: Locale) {
-
   enum class Locale(val tag: String?) {
     DEFAULT(null),
     FR("fr-rFR"),
     GB("en-rGB")
   }
 
-  @Before
-  fun setup() {
-    System.setProperty("app.cash.paparazzi.defaultLocale", locale.tag.orEmpty())
-    paparazzi.unsafeUpdateConfig(
-      deviceConfig = DeviceConfig.NEXUS_5.copy(
-        locale = System.getProperty("app.cash.paparazzi.defaultLocale")?.takeIf { it.isNotEmpty() }
-      )
-    )
-  }
-
-  @After
-  fun teardown() {
-    System.clearProperty("app.cash.paparazzi.defaultLocale")
-  }
-
   @get:Rule
-  val paparazzi = Paparazzi()
+  val chain: RuleChain = RuleChain
+    .outerRule { base, _ ->
+      object : Statement() {
+        override fun evaluate() {
+          try {
+            System.setProperty("app.cash.paparazzi.defaultLocale", locale.tag.orEmpty())
+            base.evaluate()
+          } catch (_: Exception) {
+            System.clearProperty("app.cash.paparazzi.defaultLocale")
+          }
+        }
+      }
+    }
+    .around(Paparazzi())
 
   @Test
   fun `verify system property sets default locale`() {
-    paparazzi.snapshot(view = paparazzi.inflate(R.layout.title_color))
+    Paparazzi().apply {
+      snapshot(view = inflate(R.layout.title_color))
+    }
   }
 }
