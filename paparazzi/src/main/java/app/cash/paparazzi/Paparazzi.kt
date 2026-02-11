d6f907ffd7f4054420d65d01e773f53942d1bda8
Nisha Toma <nishatoma@gmail.com>
2026-02-10 22:33:07 -0500

    Make detectMaxPercentDifferenceDefault public util method Paparazzi class

--- paparazzi/src/main/java/app/cash/paparazzi/Paparazzi.kt
+++ paparazzi/src/main/java/app/cash/paparazzi/Paparazzi.kt
@@ -21,6 +21,7 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.compose.runtime.Composable
+import app.cash.paparazzi.Paparazzi.PaparazziUtils.detectMaxPercentDifferenceDefault
 import com.android.ide.common.rendering.api.SessionParams.RenderingMode
 import org.junit.rules.TestRule
 import org.junit.runner.Description
@@ -187,8 +188,10 @@ public class Paparazzi @JvmOverloads constructor(
       } else {
         HtmlReportWriter(maxPercentDifference = maxPercentDifference)
       }
+  }
 
-    private fun detectMaxPercentDifferenceDefault(): Double =
+  public object PaparazziUtils {
+    public fun detectMaxPercentDifferenceDefault(): Double =
       System.getProperty("app.cash.paparazzi.maxPercentDifferenceDefault")?.toDoubleOrNull() ?: 0.01
   }
 }
