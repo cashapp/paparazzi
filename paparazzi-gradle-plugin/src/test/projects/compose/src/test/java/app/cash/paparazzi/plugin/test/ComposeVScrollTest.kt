cb4b2cd9fe49c9d7a353d652fbf2540b650383f8
Geoff Powell <geoffpowell13@gmail.com>
2026-09-14 15:15:45 -0400

    cleanup

--- paparazzi-gradle-plugin/src/test/projects/compose/src/test/java/app/cash/paparazzi/plugin/test/ComposeVScrollTest.kt
+++ paparazzi-gradle-plugin/src/test/projects/compose/src/test/java/app/cash/paparazzi/plugin/test/ComposeVScrollTest.kt
@@ -1,7 +1,5 @@
 package app.cash.paparazzi.plugin.test
 
-import android.content.Context
-import android.widget.FrameLayout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
