f46734d4b00299050a1e85a7f50de8965cf64d58
Simon Marquis <simon.marquis@adevinta.com>
2026-09-18 14:50:14 +0200

    fix: avoid capturing AndroidVariantSources instance in the lambda
    breaking the CC..

--- paparazzi-gradle-plugin/src/main/java/app/cash/paparazzi/gradle/AndroidVariantSources.kt
+++ paparazzi-gradle-plugin/src/main/java/app/cash/paparazzi/gradle/AndroidVariantSources.kt
@@ -25,9 +25,10 @@ internal class AndroidVariantSources(
 
   // https://android.googlesource.com/platform/tools/base/+/96015063acd3455a76cdf1cc71b23b0828c0907f/build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/MergeResources.kt#875
   val moduleResourceDirs: FileCollection by lazy {
+    val isCurrentProject = project.asCurrentProjectFilter()
     unitTest.runtimeConfiguration
       .artifactsFor(AndroidArtifacts.ArtifactType.ANDROID_RES.type) {
-        it is ProjectComponentIdentifier && !it.isCurrentProject()
+        it is ProjectComponentIdentifier && !isCurrentProject(it)
       }
       .artifactFiles
   }
@@ -44,9 +45,10 @@ internal class AndroidVariantSources(
 
   // https://android.googlesource.com/platform/tools/base/+/96015063acd3455a76cdf1cc71b23b0828c0907f/build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/MergeResources.kt#875
   val moduleAssetDirs: FileCollection by lazy {
+    val isCurrentProject = project.asCurrentProjectFilter()
     unitTest.runtimeConfiguration
       .artifactsFor(AndroidArtifacts.ArtifactType.ASSETS.type) {
-        it is ProjectComponentIdentifier && !it.isCurrentProject()
+        it is ProjectComponentIdentifier && !isCurrentProject(it)
       }
       .artifactFiles
   }
@@ -58,17 +60,22 @@ internal class AndroidVariantSources(
   }
 
   val packageAwareArtifactFiles: FileCollection by lazy {
+    val isCurrentProject = project.asCurrentProjectFilter()
     unitTest.runtimeConfiguration
-      .artifactsFor(AndroidArtifacts.ArtifactType.SYMBOL_LIST_WITH_PACKAGE_NAME.type) { !it.isCurrentProject() }
+      .artifactsFor(AndroidArtifacts.ArtifactType.SYMBOL_LIST_WITH_PACKAGE_NAME.type) { !isCurrentProject(it) }
       .artifactFiles
   }
 
-  /**
-   * Distinguishes by both build path and project path to correctly handle included builds.
-   * @return `true` if this identifier refers to the current project.
-   */
-  private fun ComponentIdentifier.isCurrentProject(): Boolean {
-    if (this !is ProjectComponentIdentifier) return false
-    return build.buildPath == project.gradle.buildPath && projectPath == project.path
+  private companion object {
+    /**
+     * Both build path and project path are needed to correctly handle composite builds.
+     * @return a filter that is `true` if the given identifier refers to the current project.
+     */
+    fun Project.asCurrentProjectFilter(): (ComponentIdentifier) -> Boolean {
+      val buildPath = gradle.buildPath
+      val projectPath = path
+      // Note: This lambda must not capture CC-incompatible types
+      return { it is ProjectComponentIdentifier && it.build.buildPath == buildPath && it.projectPath == projectPath }
+    }
   }
 }
