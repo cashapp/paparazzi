bb135274893cc8033b91e8177bc5f87151e1546a
Colin Marsch <cmarsch@squareup.com>
2026-09-24 11:19:11 -0500

    Compare AAR resources across dependency changes

--- paparazzi-gradle-plugin/src/test/java/app/cash/paparazzi/gradle/PaparazziPluginTest.kt
+++ paparazzi-gradle-plugin/src/test/java/app/cash/paparazzi/gradle/PaparazziPluginTest.kt
@@ -1327,20 +1327,16 @@ class PaparazziPluginTest {
 
     val resourcesFile = File(fixtureRoot, "build/intermediates/paparazzi/debug/resources.json")
 
-    var config = resourcesFile.loadConfig()
-    assertThat(config.aarExplodedDirs)
-      .comparingElementsUsing(MATCHES_PATTERN)
-      .containsExactly(
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/external1/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/core-1.19.1/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/annotation-experimental-1.4.1/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/core-viewtree-1.0.0/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/lifecycle-runtime/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/profileinstaller-1.4.0/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/startup-runtime-1.1.1/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/tracing-1.2.0/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/core-runtime-2.2.0/res\$"
-      )
+    fun aarResourceDirs(): List<String> =
+      resourcesFile.loadConfig().aarExplodedDirs.map { path ->
+        assertThat(path).matches("$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/[^/]+/res\$")
+        path.substringAfterLast("/transformed/")
+      }
+
+    val firstAarResourceDirs = aarResourceDirs()
+    assertThat(firstAarResourceDirs).contains("external1/res")
+    assertThat(firstAarResourceDirs).doesNotContain("external2/res")
+    assertThat(firstAarResourceDirs.size).isGreaterThan(1)
 
     buildDir.deleteRecursively()
 
@@ -1356,20 +1352,8 @@ class PaparazziPluginTest {
       assertThat(this!!.outcome).isEqualTo(SUCCESS)
     }
 
-    config = resourcesFile.loadConfig()
-    assertThat(config.aarExplodedDirs)
-      .comparingElementsUsing(MATCHES_PATTERN)
-      .containsExactly(
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/external2/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/core-1.19.1/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/annotation-experimental-1.4.1/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/core-viewtree-1.0.0/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/lifecycle-runtime/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/profileinstaller-1.4.0/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/startup-runtime-1.1.1/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/tracing-1.2.0/res\$",
-        "$GRADLE_CACHE_TRANSFORMS_PATH_REGEX/core-runtime-2.2.0/res\$"
-      )
+    assertThat(aarResourceDirs())
+      .containsExactlyElementsIn(firstAarResourceDirs - "external1/res" + "external2/res")
   }
 
   @Test
